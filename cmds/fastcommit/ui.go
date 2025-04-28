499e6c3d191f9fc693fb8bb42ed4307e8b043cdd
barry <kooksee@163.com>
2025-04-28 21:44:40 +0800

    refactor(cmds): update git message prompt text

--- cmds/fastcommit/ui.go
+++ cmds/fastcommit/ui.go
@@ -61,7 +61,7 @@ func (m model2) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View is called to draw the textinput step
 func (m model2) View() string {
-	return fmt.Sprintf("git message: %s\nPlease update and type key enter\n", m.textInput.View())
+	return fmt.Sprintf("git message(update or enter): %s", m.textInput.View())
 }
 
 func (m model2) Value() string {
