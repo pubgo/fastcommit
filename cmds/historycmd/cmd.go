c3ed99fbc845e33f6131c5e7519f6eb031751752
barry <kooksee@163.com>
2025-07-16 20:24:12 +0800

    fix: barry quick fix, 2025-07-16 20:24:12

--- cmds/historycmd/cmd.go
+++ cmds/historycmd/cmd.go
@@ -2,16 +2,12 @@ package historycmd
 
 import (
 	"context"
-	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
 	mapset "github.com/deckarep/golang-set/v2"
 	"os"
+	"strconv"
 	"strings"
 
-	tea "github.com/charmbracelet/bubbletea"
-	semver "github.com/hashicorp/go-version"
-	"github.com/pubgo/fastcommit/utils"
-	"github.com/pubgo/funk/assert"
-	"github.com/pubgo/funk/errors"
 	"github.com/pubgo/funk/recovery"
 	"github.com/samber/lo"
 	"github.com/urfave/cli/v3"
@@ -27,51 +23,27 @@ func New() *cli.Command {
 			var path = "/Users/barry/Documents/git/zshrc.history"
 			var data = lo.Must(os.ReadFile(path))
 			var set = mapset.NewSet[string]()
-			_ = set
 			for _, line := range strings.Split(string(data), "\n") {
 				lines := strings.SplitN(strings.TrimSpace(line), " ", 2)
-				if len(lines) == 2 {
-					line = lines[1]
-				} else {
-					line = lines[0]
+				if len(lines) == 2 && isNumber(strings.Trim(strings.TrimSpace(lines[0]), "*")) {
+					line = strings.TrimSpace(lines[1])
 				}
 				line = strings.TrimSpace(line)
 				set.Add(line)
 			}
 
-			for _, v := range set.ToSlice() {
-				fmt.Println(v)
-			}
-			return nil
-
-			var p = tea.NewProgram(initialModel())
-			m := assert.Must1(p.Run()).(model)
-			selected := strings.TrimSpace(m.selected)
-			if selected == "" {
-				return nil
-			}
-
-			tags := utils.GetAllGitTags()
-			ver := utils.GetNextTag(selected, tags)
-			if selected == envRelease {
-				ver = utils.GetNextReleaseTag(tags)
-			}
-
-			tagName := "v" + strings.TrimPrefix(ver.Original(), "v")
-			var p1 = tea.NewProgram(InitialTextInputModel(tagName))
-			m1 := assert.Must1(p1.Run()).(model2)
-			if m1.exit {
-				return nil
-			}
-
-			tagName = m1.Value()
-			_, err := semver.NewVersion(tagName)
-			if err != nil {
-				return errors.Format("tag name is not valid: %s", tagName)
-			}
-
-			utils.GitPushTag(tagName)
+			p := tea.NewProgram(initialModel(set.ToSlice()))
+			_ = lo.Must(p.Run())
 			return nil
 		},
 	}
+}
+
+func isNumber(str string) bool {
+	if str == "" {
+		return false
+	}
+
+	_, err := strconv.Atoi(str)
+	return err == nil
 }
--- cmds/historycmd/ui.go
+++ cmds/historycmd/ui.go
@@ -2,179 +2,115 @@ package historycmd
 
 import (
 	"fmt"
+	"github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/samber/lo"
+	"strings"
 
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	semver "github.com/hashicorp/go-version"
-	"github.com/pubgo/funk/log"
 )
 
-const (
-	envAlpha   = "alpha"
-	envBeta    = "beta"
-	envRelease = "release"
+const gap = "\n\n"
+
+type (
+	errMsg error
 )
 
 type model struct {
-	cursor   int
-	choices  []string
-	selected string
-	length   int
+	viewport    viewport.Model
+	messages    []string
+	textarea    textinput.Model
+	senderStyle lipgloss.Style
+	err         error
 }
 
-func initialModel() model {
-	choices := []string{envAlpha, envBeta, envRelease}
-	return model{
-		choices: choices,
-		length:  len(choices),
+func initialModel(messages []string) *model {
+	ta := textinput.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	//ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return &model{
+		textarea:    ta,
+		messages:    messages,
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
 	}
 }
 
-func (m model) Init() tea.Cmd { return nil }
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-		case tea.KeyUp, tea.KeyLeft, tea.KeyDown, tea.KeyRight:
-			m.cursor++
-		case tea.KeyEnter:
-			m.selected = m.choices[m.cursor%m.length]
-			return m, tea.Quit
-		default:
-			log.Error().Str("key", msg.String()).Msg("unknown key")
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
+func (m *model) Init() tea.Cmd {
+	return textarea.Blink
 }
 
-func (m model) View() string {
-	s := "Please Select Pre Tag:\n"
-
-	for i, choice := range m.choices {
-		cursor := " "
-		if m.cursor%m.length == i {
-			cursor = ">"
-		}
-
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
-	}
-
-	return s
-}
-
-type model1 struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
-}
-
-func InitialModelNew() model1 {
-	s := spinner.New()
-	s.Spinner = spinner.Line
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model1{spinner: s}
-}
-
-func (m model1) Init() tea.Cmd {
-	return m.spinner.Tick
-}
-
-func (m model1) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-		default:
-			return m, nil
-		}
-
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-	}
-}
-
-func (m model1) View() string {
-
-	if m.err != nil {
-		return m.err.Error()
-	}
-	str := fmt.Sprintf("%s Preparing...", m.spinner.View())
-	if m.quitting {
-		return str + "\n"
-	}
-	return str
-}
-
-type model2 struct {
-	textInput textinput.Model
-	exit      bool
-}
-
-// sanitizeInput verifies that an input text string gets validated
-func sanitizeInput(input string) error {
-	_, err := semver.NewSemver(input)
-	return err
-}
-
-func InitialTextInputModel(data string) model2 {
-	ti := textinput.New()
-	ti.Focus()
-	ti.Prompt = ""
-	ti.CharLimit = 156
-	ti.Width = 20
-	ti.Validate = sanitizeInput
-	ti.SetValue(data)
-
-	return model2{
-		textInput: ti,
-	}
-}
-
-// Init is called at the beginning of a textinput step
-// and sets the cursor to blink
-func (m model2) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-// Update is called when "things happen", it checks for the users text input,
-// and for Ctrl+C or Esc to close the program.
-func (m model2) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-			return m, tea.Quit
-		case tea.KeyCtrlC, tea.KeyEsc:
-			m.exit = true
-			return m, tea.Quit
-		}
-	}
-
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
-}
-
-// View is called to draw the textinput step
-func (m model2) View() string {
-	return fmt.Sprintf(
-		"new tag: %s\n",
-		m.textInput.View(),
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
 	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		//m.textarea.SetWidth(msg.Width)
+		m.viewport.Height = msg.Height - lipgloss.Height(gap)
+
+		messages := m.messages
+		if len(messages) > m.viewport.Height {
+			messages = messages[:m.viewport.Height]
+		}
+
+		if len(messages) > 0 {
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(messages, "\n")))
+		}
+		m.viewport.GotoBottom()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			messages := m.messages
+
+			val := strings.TrimSpace(m.textarea.Value())
+			if val != "" {
+				messages = lo.Filter(messages, func(item string, index int) bool { return fuzzy.MatchFold(val, item) })
+			}
+
+			if len(messages) > m.viewport.Height {
+				messages = messages[:m.viewport.Height]
+			}
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(messages, "\n")))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
 }
 
-func (m model2) Value() string {
-	return m.textInput.Value()
+func (m *model) View() string {
+	return fmt.Sprintf(
+		"%s%s%s",
+		m.viewport.View(),
+		gap,
+		m.textarea.View(),
+	)
 }
